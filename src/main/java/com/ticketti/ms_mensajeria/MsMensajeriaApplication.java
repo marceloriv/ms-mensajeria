903333a38aaf313503163171e4aae5bfa7777cf3
marceloriv <ma.riveracontreras@duocuc.cl>
2026-04-15 17:43:47 -0400

    feat: :tada: primer commit

---
+++ src/main/java/com/ticketti/ms_mensajeria/MsMensajeriaApplication.java
@@ -0,0 +1,13 @@
+package com.ticketti.ms_mensajeria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsMensajeriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsMensajeriaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ticketti/ms_mensajeria/MsMensajeriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ticketti.ms_mensajeria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsMensajeriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
