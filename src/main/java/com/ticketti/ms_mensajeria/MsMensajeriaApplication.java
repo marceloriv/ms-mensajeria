9cb2b8af061c0819eb6ea79f333c30704ca16e47
Pamela Alvarez <palvarezlara@gmail.com>
2026-05-08 20:27:44 -0400

    chore(mensajeria): estructura inicial de paquetes

--- src/main/java/com/ticketti/ms_mensajeria/MsMensajeriaApplication.java
+++ src/main/java/com/ticketti/ms_mensajeria/MsMensajeriaApplication.java
@@ -2,12 +2,22 @@ package com.ticketti.ms_mensajeria;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+/**
+ * Microservicio MS-Mensajería.
+ * Responsable de:
+ * - Generar el código QR único por entrada comprada
+ * - Enviar el correo de confirmación al comprador
+ * - Escuchar el evento pago.aprobado desde RabbitMQ
+ *
+ * Patrón aplicado: Factory Method (NotificationFactory)
+ */
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MsMensajeriaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MsMensajeriaApplication.class, args);
 	}
-
 }
